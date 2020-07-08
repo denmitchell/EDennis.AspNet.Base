c716171eed98961f47d48637a6127e48f8a3ec19
Dennis Mitchell <den.mitchell@yahoo.com>
2020-07-08 05:39:36 -0400

    Extend RoleManager and UserManager

--- EDennis.AspNet.Base/Security/AspNetIdentity/DomainRoleManager.cs
+++ EDennis.AspNet.Base/Security/AspNetIdentity/DomainRoleManager.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace EDennis.AspNet.Base.Security {
@@ -17,28 +18,70 @@ namespace EDennis.AspNet.Base.Security {
             ILogger<RoleManager<TRole>> logger) : base(store, roleValidators, keyNormalizer, errors, logger) {
         }
 
-        public override Task<IdentityResult> CreateAsync(TRole role) {
-            UpdateName(role);
-            return base.CreateAsync(role);
+        public override async Task<IdentityResult> CreateAsync(TRole role) {
+            await UpdateNameAsync(role);
+            return await base.CreateAsync(role);
         }
 
-        public override Task<IdentityResult> UpdateAsync(TRole role) {
-            UpdateName(role);
-            return base.UpdateAsync(role);
-        }
-
-        protected override Task<IdentityResult> UpdateRoleAsync(TRole role) {
-            UpdateName(role);
-            return base.UpdateRoleAsync(role);
+        public override async Task<IdentityResult> UpdateAsync(TRole role) {
+            await UpdateNameAsync(role);
+            return await base.UpdateAsync(role);
         }
 
 
-        public virtual void UpdateName(TRole role) {
+
+        public override async Task<IdentityResult> SetRoleNameAsync(TRole role, string name) {
+            var components = name.Split('@');
+            if (components.Length == 1) {
+                role.ApplicationId = default;
+                role.OrganizationId = default;
+                role.RoleName = role.Name;
+                return await base.SetRoleNameAsync(role, name);
+            } else {
+                var result = await base.SetRoleNameAsync(role, name);
+
+                if (!result.Succeeded)
+                    return result;
+
+                if (!(Store is RoleStore<TRole> store))
+                    throw new Exception("Cannot use DomainRoleManager.SetRoleNameAsync without Microsoft.AspNetCore.Identity.EntityFrameworkCore.RoleStore<TRole> where TRole : DomainRole.");
+
+                var db = store.Context.Database;
+
+                if (components.Length == 3)
+                    await db.ExecuteSqlInterpolatedAsync($@"
+update r 
+  set RoleName = {components[0]}, OrganizationId = o.Id, ApplicationId = a.Id
+  from AspNetRoles r
+  inner join AspNetOrganizations o
+    on o.Name = {components[1]}
+  inner join AspNetApplications a
+    on a.Name = {components[2]}
+  where r.Id = {role.Id}");
+
+                if (components.Length == 2)
+                    await db.ExecuteSqlInterpolatedAsync($@"
+update r 
+  set RoleName = {components[0]}, OrganizationId = o.Id, ApplicationId = a.Id
+  from AspNetRoles r
+  left outer join AspNetOrganizations o
+    on o.Name = {components[1]}
+  left outer join AspNetApplications a
+    on a.Name = {components[1]}
+  where r.Id = {role.Id}");
+
+            }
+
+            return IdentityResult.Success;
+        }
+
+
+        public virtual async Task UpdateNameAsync(TRole role) {
             if (role.RoleName == null || (role.OrganizationId == default && role.ApplicationId == default))
                 return;
 
             if (!(Store is RoleStore<TRole> store))
-                throw new Exception("Cannot use DomainRoleManager without Microsoft.AspNetCore.Identity.EntityFrameworkCore.RoleStore<TRole> where TRole : DomainRole.");
+                throw new Exception("Cannot use DomainRoleManager.UpdateNameAsync without Microsoft.AspNetCore.Identity.EntityFrameworkCore.RoleStore<TRole> where TRole : DomainRole.");
 
             var db = store.Context.Database;
 
@@ -47,7 +90,7 @@ namespace EDennis.AspNet.Base.Security {
             //to the way that the generic classes are composed.
 
             if (role.OrganizationId != default && role.ApplicationId != default)
-                db.ExecuteSqlInterpolated( $@"
+                await db.ExecuteSqlInterpolatedAsync( $@"
 update r 
   set Role = r.RoleName + '@' + o.Name + '@' + a.Name 
   from AspNetRoles r
@@ -58,7 +101,7 @@ update r
   where r.Id = {role.Id}");
             
             else if (role.OrganizationId != default)
-                db.ExecuteSqlInterpolated($@"
+                await db.ExecuteSqlInterpolatedAsync($@"
 update r 
   set Role = r.RoleName + '@' + o.Name 
   from AspNetRoles r
@@ -67,7 +110,7 @@ update r
   where r.Id = {role.Id}");
 
             else
-                db.ExecuteSqlInterpolated($@"
+                await db.ExecuteSqlInterpolatedAsync($@"
 update r 
   set Role = r.RoleName + '@' + a.Name 
   from AspNetRoles r
@@ -76,6 +119,75 @@ update r
   where r.Id = {role.Id}");
 
         }
+
+
+
+        public virtual async Task<IEnumerable<TRole>> GetRolesForApplicationAsync(string applicationName) {
+
+            
+
+            if (!(Store is RoleStore<TRole> store))
+                throw new Exception("Cannot use DomainRoleManager.GetRolesForApplicationAsync(string applicationName) without Microsoft.AspNetCore.Identity.EntityFrameworkCore.RoleStore<TRole> where TRole : DomainRole.");
+
+
+            var qry = store.Context.Set<TRole>()
+                .FromSqlInterpolated($@"
+select r.* 
+  from AspNetRoles r
+  inner join AspNetApplications a
+    on a.Id = r.ApplicationId
+  where a.Name = {applicationName}
+            ").AsNoTracking();
+
+            return await qry.ToListAsync();
+        }
+
+
+        public virtual async Task<IEnumerable<TRole>> GetRolesForApplicationAsync(int applicationId) {
+
+            if(!SupportsQueryableRoles)
+                throw new Exception("Cannot use DomainRoleManager.GetRolesForApplicationAsync(int applicationId) without Queryable Roles.");
+
+            var qry = Roles
+                .Where(r => r.ApplicationId == applicationId)
+                .AsNoTracking();
+
+            return await qry.ToListAsync();
+        }
+
+
+        public virtual async Task<IEnumerable<TRole>> GetRolesForOrganizationAsync(string organizationName) {
+
+            if (!(Store is RoleStore<TRole> store))
+                throw new Exception("Cannot use DomainRoleManager.GetRolesForOrganizationAsync(string organizationName) without Microsoft.AspNetCore.Identity.EntityFrameworkCore.RoleStore<TRole> where TRole : DomainRole.");
+
+            var qry = store.Context.Set<TRole>()
+                .FromSqlInterpolated($@"
+select r.* 
+  from AspNetRoles r
+  inner join AspNetOrganizations o
+    on o.Id = r.OrganizationId
+  where o.Name = {organizationName}
+            ").AsNoTracking();
+
+            return await qry.ToListAsync();
+        }
+
+
+        public virtual async Task<IEnumerable<TRole>> GetRolesForOrganizationAsync(int organizationId) {
+
+            if (!SupportsQueryableRoles)
+                throw new Exception("Cannot use DomainRoleManager.GetRolesForOrganizationAsync(int organizationId) without Queryable Roles.");
+
+            var qry = Roles
+                .Where(r => r.OrganizationId == organizationId)
+                .AsNoTracking();
+
+            return await qry.ToListAsync();
+        }
+
+
+
 
     }
 
---
+++ EDennis.AspNet.Base/Security/AspNetIdentity/DomainUserManager.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDennis.AspNet.Base.Security.AspNetIdentity {
+    public class DomainUserManager<TUser> : UserManager<TUser> 
+        where TUser: DomainUser, new() {
+        public DomainUserManager(IUserStore<TUser> store, IOptions<IdentityOptions> optionsAccessor, 
+            IPasswordHasher<TUser> passwordHasher, IEnumerable<IUserValidator<TUser>> userValidators, 
+            IEnumerable<IPasswordValidator<TUser>> passwordValidators, 
+            ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, 
+            IServiceProvider services, ILogger<UserManager<TUser>> logger) 
+            : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger) {
+        }
+
+        public virtual async Task<IEnumerable<TUser>> GetUsersForOrganizationAsync(int organizationId, int pageNumber = 1, int pageSize = 100) {
+            if(!SupportsQueryableUsers)
+                throw new Exception("Cannot use DomainUserManager.GetUsersForOrganizationAsync(int organizationId, int pageNumber, int pageSize) without Queryable Users.");
+
+            var (skip, take) = ((pageNumber - 1) * pageSize, pageSize);
+
+            var qry = Users
+                .AsNoTracking()
+                .Where(u => u.OrganizationId == organizationId)
+                .Skip(skip)
+                .Take(take);
+
+            return await qry.ToListAsync();
+                
+        }
+
+        public virtual async Task<IEnumerable<TUser>> GetUsersForOrganizationAsync(string organizationName) {
+
+
+            if (!(Store is UserStore<TUser> store))
+                throw new Exception("Cannot use DomainUserManager.GetUsersForOrganizationAsync(string organizationName) without Microsoft.AspNetCore.Identity.EntityFrameworkCore.UserStore<TUser> where TUser : DomainUser.");
+
+            var qry = store.Context.Set<TUser>()
+                .FromSqlInterpolated($@"
+select u.* 
+  from AspNetUsers u
+  inner join AspNetOrganizations o
+    on o.Id = u.OrganizationId
+  where o.Name = {organizationName}
+            ").AsNoTracking();
+
+            return await qry.ToListAsync();
+
+        }
+
+        public virtual async Task<IEnumerable<TUser>> GetUsersForOrganizationAsync(string organizationName, int pageNumber, int pageSize) {
+
+            var (skip, take) = ((pageNumber - 1) * pageSize, pageSize);
+
+            if (!(Store is UserStore<TUser> store))
+                throw new Exception("Cannot use DomainUserManager.GetUsersForOrganizationAsync(string organizationName, int pageNumber, int pageSize) without Microsoft.AspNetCore.Identity.EntityFrameworkCore.UserStore<TUser> where TUser : DomainUser.");
+
+            if(!store.Context.Database.ProviderName.Contains("SqlServer") && !store.Context.Database.ProviderName.Contains("Oracle"))
+                throw new Exception("Cannot use DomainUserManager.GetUsersForOrganizationAsync(string organizationName, int pageNumber, int pageSize) without SqlServer or Oracle providers");
+
+            var qry = store.Context.Set<TUser>()
+                .FromSqlInterpolated($@"
+select u.* 
+  from AspNetUsers u
+  inner join AspNetOrganizations o
+    on o.Id = u.OrganizationId
+  where o.Name = {organizationName}
+  offset {skip} rows
+  fetch next {take} rows only
+            ").AsNoTracking();
+
+            return await qry.ToListAsync();
+
+        }
+
+        public virtual async Task<IEnumerable<TUser>> GetUsersForApplicationAsync(string applicationName) {
+
+            if (!(Store is UserStore<TUser> store))
+                throw new Exception("Cannot use DomainUserManager.GetUsersForApplicationAsync(string applicationName) without Microsoft.AspNetCore.Identity.EntityFrameworkCore.UserStore<TUser> where TUser : DomainUser.");
+
+            var qry = store.Context.Set<TUser>()
+                .FromSqlInterpolated($@"
+select u.* 
+  from AspNetUsers u
+  where exists (
+    select 0
+      from AspNetRoles r
+      inner join AspNetUserRoles ur
+        on r.Id = ur.RoleId
+      inner join AspNetApplications a
+        on r.ApplicationId = a.Id
+      where a.Name = {applicationName}
+        and ur.UserId = u
+  )").AsNoTracking();
+
+            return await qry.ToListAsync();
+
+        }
+
+
+
+        public virtual async Task<IEnumerable<TUser>> GetUsersForApplicationAsync(string applicationName, int pageNumber, int pageSize) {
+
+            var (skip, take) = ((pageNumber - 1) * pageSize, pageSize);
+
+            if (!(Store is UserStore<TUser> store))
+                throw new Exception("Cannot use DomainUserManager.GetUsersForApplicationAsync(string applicationName, int pageNumber, int pageSize) without Microsoft.AspNetCore.Identity.EntityFrameworkCore.UserStore<TUser> where TUser : DomainUser.");
+
+            if (!store.Context.Database.ProviderName.Contains("SqlServer") && !store.Context.Database.ProviderName.Contains("Oracle"))
+                throw new Exception("Cannot use DomainUserManager.GetUsersForApplicationAsync(string applicationName, int pageNumber, int pageSize) without SqlServer or Oracle providers");
+
+            var qry = store.Context.Set<TUser>()
+                .FromSqlInterpolated($@"
+select u.* 
+  from AspNetUsers u
+  where exists (
+    select 0
+      from AspNetRoles r
+      inner join AspNetUserRoles ur
+        on r.Id = ur.RoleId
+      inner join AspNetApplications a
+        on r.ApplicationId = a.Id
+      where a.Name = {applicationName}
+        and ur.UserId = u
+  )
+  offset {skip} rows
+  fetch next {take} rows only").AsNoTracking();
+
+            return await qry.ToListAsync();
+
+        }
+
+
+        public virtual async Task<IEnumerable<TUser>> GetUsersForApplicationAsync(int applicationId) {
+
+            if (!(Store is UserStore<TUser> store))
+                throw new Exception("Cannot use DomainUserManager.GetUsersForApplicationAsync(int applicationId) without Microsoft.AspNetCore.Identity.EntityFrameworkCore.UserStore<TUser> where TUser : DomainUser.");
+
+            var qry = store.Context.Set<TUser>()
+                .FromSqlInterpolated($@"
+select u.* 
+  from AspNetUsers u
+  where exists (
+    select 0
+      from AspNetRoles r
+      inner join AspNetUserRoles ur
+        on r.Id = ur.RoleId
+      where r.ApplicationId = {applicationId}
+        and ur.UserId = u
+  )").AsNoTracking();
+
+            return await qry.ToListAsync();
+
+        }
+
+
+
+        public virtual async Task<IEnumerable<TUser>> GetUsersForApplicationAsync(int applicationId, int pageNumber, int pageSize) {
+
+            var (skip, take) = ((pageNumber - 1) * pageSize, pageSize);
+
+            if (!(Store is UserStore<TUser> store))
+                throw new Exception("Cannot use DomainUserManager.GetUsersForApplicationAsync(int applicationId, int pageNumber, int pageSize) without Microsoft.AspNetCore.Identity.EntityFrameworkCore.UserStore<TUser> where TUser : DomainUser.");
+
+            if (!store.Context.Database.ProviderName.Contains("SqlServer") && !store.Context.Database.ProviderName.Contains("Oracle"))
+                throw new Exception("Cannot use DomainUserManager.GetUsersForApplicationAsync(int applicationId, int pageNumber, int pageSize) without SqlServer or Oracle providers");
+
+            var qry = store.Context.Set<TUser>()
+                .FromSqlInterpolated($@"
+select u.* 
+  from AspNetUsers u
+  where exists (
+    select 0
+      from AspNetRoles r
+      inner join AspNetUserRoles ur
+        on r.Id = ur.RoleId
+      where r.ApplicationId = {applicationId}
+        and ur.UserId = u
+  )
+  offset {skip} rows
+  fetch next {take} rows only").AsNoTracking();
+
+            return await qry.ToListAsync();
+
+        }
+
+
+    }
+}
