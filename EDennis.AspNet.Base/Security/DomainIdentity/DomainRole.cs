2c2523c198b7c33a6443fe16c36d7d69c1034193
Dennis Mitchell <den.mitchell@yahoo.com>
2020-07-11 03:21:21 -0400

    remove ClaimsPrincipalFactory and Add DomainRole.AppliesTo

--- EDennis.AspNet.Base/Security/DomainIdentity/DomainRole.cs
+++ EDennis.AspNet.Base/Security/DomainIdentity/DomainRole.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace EDennis.AspNet.Base.Security {
     public class DomainRole : IdentityRole<Guid> {
 
+        [RegularExpression(@"[U|A|O]")]
+        public virtual char AppliesTo { get; set; } = 'U';
         public virtual Guid? OrganizationId { get; set; }
         public virtual Guid? ApplicationId { get; set; }
         public virtual string Title { get; set; }
