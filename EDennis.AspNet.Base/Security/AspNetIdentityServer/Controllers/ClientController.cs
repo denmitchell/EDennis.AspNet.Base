0c4ca4e430e417d510224a3cfc346f8d3b759259
Dennis Mitchell <den.mitchell@yahoo.com>
2020-07-16 05:02:59 -0400

    ongoing work on ClientController Patch method

--- EDennis.AspNet.Base/Security/AspNetIdentityServer/Controllers/ClientController.cs
+++ EDennis.AspNet.Base/Security/AspNetIdentityServer/Controllers/ClientController.cs
@@ -9,7 +9,8 @@ using System;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
-using EDennis.AspNet.Base.Security.PatchExtensions;
+using IdentityModel;
+using System.Collections.Generic;
 
 namespace EDennis.AspNet.Base.Security {
 
@@ -83,72 +84,245 @@ namespace EDennis.AspNet.Base.Security {
 
 
         [HttpPatch("{clientId}")]
-        public async Task<IActionResult> PatchAsync([FromBody] JsonElement partialModel, [FromRoute] string clientId) {
+        public async Task<IActionResult> PatchAsync([FromBody] JsonElement partialModel, 
+            [FromRoute] string clientId, [FromQuery] bool mergeCollections = true) {
 
             var existing = _dbContext.Clients.FirstOrDefault(a => a.ClientId == clientId);
             if (existing == null)
                 return NotFound();
 
+            var model = existing.ToModel();
 
             foreach (var prop in partialModel.EnumerateObject()) {
                 try {
                     switch (prop.Name) {
                         case "AbsoluteRefreshTokenLifetime":
                         case "absoluteRefreshTokenLifetime":
-                            existing.AbsoluteRefreshTokenLifetime = prop.Value.GetInt32();
+                            model.AbsoluteRefreshTokenLifetime = prop.Value.GetInt32();
                             break;
                         case "AccessTokenLifetime":
                         case "accessTokenLifetime":
-                            existing.AccessTokenLifetime = prop.Value.GetInt32();
+                            model.AccessTokenLifetime = prop.Value.GetInt32();
                             break;
                         case "AccessTokenType":
                         case "accessTokenType":
-                            existing.AccessTokenType = prop.Value.GetInt32();
+                            model.AccessTokenType = (M.AccessTokenType)Enum.Parse(typeof(M.AccessTokenType),prop.Value.GetString());
                             break;
                         case "AllowAccessTokensViaBrowser":
                         case "allowAccessTokensViaBrowser":
-                            existing.AllowAccessTokensViaBrowser = prop.Value.GetBoolean();
+                            model.AllowAccessTokensViaBrowser = prop.Value.GetBoolean();
                             break;
                         case "AllowedCorsOrigins":
                         case "allowedCorsOrigins":
-                            existing.AllowedCorsOrigins = prop.Value.EnumerateArray().Select(e =>
-                                new ClientCorsOrigin {
-                                    Origin = e.GetString()
-                                }).ToList();
+                            var allowedCorsOrigins = prop.Value.EnumerateArray().Select(e => e.GetString()).ToArray();
+                            if (mergeCollections && model.AllowedCorsOrigins != null)
+                                model.AllowedCorsOrigins = model.AllowedCorsOrigins.Union(allowedCorsOrigins).ToArray();
+                            else
+                                model.AllowedCorsOrigins = allowedCorsOrigins.ToArray();
                             break;
                         case "AllowedGrantTypes":
                         case "allowedGrantTypes":
-                            existing.AllowedGrantTypes = prop.Value.EnumerateArray().Select(e =>
-                                new ClientGrantType {
-                                    GrantType = e.GetString()
-                                }).ToList();
+                            var allowedGrantTypes = prop.Value.EnumerateArray().Select(e => e.GetString()).ToArray();
+                            if (mergeCollections && model.AllowedGrantTypes != null)
+                                model.AllowedGrantTypes = model.AllowedGrantTypes.Union(allowedGrantTypes).ToArray();
+                            else
+                                model.AllowedGrantTypes = allowedGrantTypes.ToArray();
                             break;
                         case "AllowedScopes":
                         case "allowedScopes":
-                            existing.AllowedScopes = prop.Value.EnumerateArray().Select(e =>
-                                new ClientScope {
-                                    Scope = e.GetString()
-                                }).ToList();
+                            var allowedScopes = prop.Value.EnumerateArray().Select(e => e.GetString()).ToArray();
+                            if (mergeCollections && model.AllowedScopes != null)
+                                model.AllowedScopes = model.AllowedScopes.Union(allowedScopes).ToArray();
+                            else
+                                model.AllowedScopes = allowedScopes.ToArray();
                             break;
                         case "AllowOfflineAccess":
                         case "allowOfflineAccess":
-                            existing.AllowOfflineAccess = prop.Value.GetBoolean();
+                            model.AllowOfflineAccess = prop.Value.GetBoolean();
                             break;
                         case "AllowPlainTextPkce":
                         case "allowPlainTextPkce":
-                            existing.AllowPlainTextPkce = prop.Value.GetBoolean();
+                            model.AllowPlainTextPkce = prop.Value.GetBoolean();
                             break;
                         case "AllowRememberConsent":
                         case "allowRememberConsent":
-                            existing.AllowRememberConsent = prop.Value.GetBoolean();
+                            model.AllowRememberConsent = prop.Value.GetBoolean();
                             break;
                         case "AlwaysIncludeUserClaimsInIdToken":
                         case "alwaysIncludeUserClaimsInIdToken":
-                            existing.AlwaysIncludeUserClaimsInIdToken = prop.Value.GetBoolean();
+                            model.AlwaysIncludeUserClaimsInIdToken = prop.Value.GetBoolean();
                             break;
                         case "AlwaysSendClientClaims":
                         case "alwaysSendClientClaims":
-                            existing.AlwaysSendClientClaims = prop.Value.GetBoolean();
+                            model.AlwaysSendClientClaims = prop.Value.GetBoolean();
+                            break;
+                        case "AuthorizationCodeLifetime":
+                        case "authorizationCodeLifetime":
+                            model.AuthorizationCodeLifetime = prop.Value.GetInt32();
+                            break;
+                        case "BackChannelLogoutSessionRequired":
+                        case "backChannelLogoutSessionRequired":
+                            model.BackChannelLogoutSessionRequired = prop.Value.GetBoolean();
+                            break;
+                        case "BackChannelLogoutUri":
+                        case "backChannelLogoutUri":
+                            model.BackChannelLogoutUri = prop.Value.GetString();
+                            break;
+                        case "Claims":
+                        case "claims":
+                            var claims = prop.Value.EnumerateArray().Select(e => {
+                                var claim = new M.ClientClaim(); 
+                                foreach (var prop2 in e.EnumerateObject()) {
+                                    switch (prop2.Name) {
+                                        case "Type":
+                                        case "type":
+                                            claim.Type = prop2.Value.GetString();
+                                            break;
+                                        case "Value":
+                                        case "value":
+                                            claim.Value = prop2.Value.GetString();
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                }
+                                return claim;
+                            });
+                            if (mergeCollections && model.Claims != null)
+                                model.Claims = model.Claims.Union(claims).ToArray();
+                            else
+                                model.Claims = claims.ToArray();
+                            break;
+                        case "ClientClaimsPrefix":
+                        case "clientClaimsPrefix":
+                            model.ClientClaimsPrefix = prop.Value.GetString();
+                            break;
+                        case "ClientId":
+                        case "clientId":
+                            model.ClientId = prop.Value.GetString();
+                            break;
+                        case "ClientName":
+                        case "clientName":
+                            model.ClientName = prop.Value.GetString();
+                            break;
+                        case "ClientSecrets":
+                        case "clientSecrets":
+                            var secrets = prop.Value.EnumerateArray().Select(e => {
+                                var secret = new M.Secret {
+                                    Type = "SharedSecret",
+                                    Expiration = DateTime.MaxValue
+                                };
+                                foreach (var prop2 in e.EnumerateObject()) {
+                                    switch (prop2.Name) {
+                                        case "Value":
+                                        case "value":
+                                            secret.Value = prop2.Value.GetString().ToSha256();
+                                            break;
+                                        case "Type":
+                                        case "type":
+                                            secret.Type = prop2.Value.GetString();
+                                            break;
+                                        case "Description":
+                                        case "description":
+                                            secret.Description = prop2.Value.GetString();
+                                            break;
+                                        case "Expiration":
+                                        case "expiration":
+                                            secret.Expiration = prop2.Value.GetDateTime();
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                }
+                                return secret;
+                            });
+                            if (mergeCollections && model.ClientSecrets != null)
+                                model.ClientSecrets = model.ClientSecrets.Union(secrets).ToArray();
+                            else
+                                model.ClientSecrets = secrets.ToArray();
+                            break;
+                        case "ClientUri":
+                        case "clientUri":
+                            model.ClientUri = prop.Value.GetString();
+                            break;
+                        case "ConsentLifetime":
+                        case "consentLifetime":
+                            model.ConsentLifetime = prop.Value.GetInt32();
+                            break;
+                        case "Description":
+                        case "description":
+                            model.Description = prop.Value.GetString();
+                            break;
+                        case "DeviceCodeLifetime":
+                        case "deviceCodeLifetime":
+                            model.DeviceCodeLifetime = prop.Value.GetInt32();
+                            break;
+                        case "Enabled":
+                        case "enabled":
+                            model.Enabled = prop.Value.GetBoolean();
+                            break;
+                        case "EnableLocalLogin":
+                        case "enableLocalLogin":
+                            model.EnableLocalLogin = prop.Value.GetBoolean();
+                            break;
+                        case "FrontChannelLogoutSessionRequired":
+                        case "frontChannelLogoutSessionRequired":
+                            model.FrontChannelLogoutSessionRequired = prop.Value.GetBoolean();
+                            break;
+                        case "FrontChannelLogoutUri":
+                        case "frontChannelLogoutUri":
+                            model.FrontChannelLogoutUri = prop.Value.GetString();
+                            break;
+                        case "IdentityProviderRestrictions":
+                        case "identityProviderRestrictions":
+                            var identityProviderRestrictions = prop.Value.EnumerateArray().Select(e => e.GetString()).ToArray();
+                            if (mergeCollections && model.IdentityProviderRestrictions != null)
+                                model.IdentityProviderRestrictions = model.IdentityProviderRestrictions.Union(identityProviderRestrictions).ToArray();
+                            else
+                                model.IdentityProviderRestrictions = identityProviderRestrictions.ToArray();
+                            break;
+                        case "IdentityTokenLifetime":
+                        case "identityTokenLifetime":
+                            model.IdentityTokenLifetime = prop.Value.GetInt32();
+                            break;
+                        case "IncludeJwtId":
+                        case "includeJwtId":
+                            model.IncludeJwtId = prop.Value.GetBoolean();
+                            break;
+                        case "LogoUri":
+                        case "logoUri":
+                            model.LogoUri = prop.Value.GetString();
+                            break;
+                        case "PairWiseSubjectSalt":
+                        case "pairWiseSubjectSalt":
+                            model.PairWiseSubjectSalt = prop.Value.GetString();
+                            break;
+                        case "PostLogoutRedirectUris":
+                        case "postLogoutRedirectUris":
+                            var postLogoutRedirectUris = prop.Value.EnumerateArray().Select(e => e.GetString()).ToArray();
+                            if (mergeCollections && model.PostLogoutRedirectUris != null)
+                                model.PostLogoutRedirectUris = model.PostLogoutRedirectUris.Union(postLogoutRedirectUris).ToArray();
+                            else
+                                model.PostLogoutRedirectUris = postLogoutRedirectUris.ToArray();
+                            break;
+                        case "Properties":
+                        case "properties":
+                            var properties = new Dictionary<string, string>();
+                            prop.Value.EnumerateObject().ToList().ForEach(e => {
+                                properties.Add(e.Name, e.Value.GetString());
+                            });
+                            if (mergeCollections && model.Properties != null)
+                                foreach (var entry in properties)
+                                    if (model.Properties.ContainsKey(entry.Key))
+                                        model.Properties[entry.Key] = entry.Value;
+                                    else
+                                        model.Properties.Add(entry.Key, entry.Value);
+                            else
+                                model.Properties = properties;
+                            break;
+                        case "ProtocolType":
+                        case "protocolType":
+                            model.ProtocolType = prop.Value.GetString();
                             break;
                         default:
                             break;
