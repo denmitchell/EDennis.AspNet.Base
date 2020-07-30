b16bfcc2ee41b6ae84fc4296c92a81bbc0213150
Dennis Mitchell <den.mitchell@yahoo.com>
2020-07-30 07:52:12 -0400

    Begin work on token service

---
+++ EDennis.AspNet.Base/Security/AspNetIdentityServer/Services/ClientCredentialsTokenService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDennis.AspNet.Base.Security {
+    public class ClientCredentialsTokenService {
+
+        //public ClientCredentitalsTokenService(IOptionsMonitor) {
+
+        //}
+
+    }
+}
