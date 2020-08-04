a120fd64f87da2d0c2034ef09b994c646160b4e6
Dennis Mitchell <den.mitchell@yahoo.com>
2020-08-04 03:35:28 -0400

    Added ProxyClient/ProxyClients for configuration of API Clients and AddProxyClients<TTokenService> extension method -- for simple configuration of the clients.

---
+++ EDennis.NetStandard.Base/Controllers/ProxyControllers/ProxyClients.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace EDennis.NetStandard.Base {
+
+    /// <summary>
+    /// Note: string key should be the name of the ProxyController
+    /// (minus "Controller")
+    /// </summary>
+    public class ProxyClients : Dictionary<string, ProxyClient> { }
+
+    public class ProxyClient {
+        public string TargetUrl { get; set; }
+    }
+}
---
+++ EDennis.NetStandard.Base/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+
+namespace EDennis.NetStandard.Base.Extensions {
+    
+    
+    public static class IServiceCollectionExtensions {
+
+        /// <summary>
+        /// Configures the TokenService and the HttpClient for all ProxyQueryControllers
+        /// and ProxyCrudControllers.
+        /// Note: This extension method requires the existence of a top-level configuration
+        /// section called "ProxyClients" that binds to a Dictionary<string,ProxyClient>
+        /// and whose key is the name of the Proxy Controller, minus the word Controller.
+        /// </summary>
+        public static IServiceCollection AddProxyClients<TTokenService>(this IServiceCollection services,
+            IConfiguration config)
+            where TTokenService : class, ITokenService {
+
+            services.TryAddSingleton<ITokenService, TTokenService>();
+
+            var clients = new ProxyClients();
+            config.GetSection("ProxyClients").Bind(clients);
+
+            foreach(var client in clients) {
+                var clientName = client.Key;
+                services.AddHttpClient(clientName, options => {
+                    options.BaseAddress = new Uri(client.Value.TargetUrl);
+                });
+            }
+
+            return services;
+        }
+    }
+}
